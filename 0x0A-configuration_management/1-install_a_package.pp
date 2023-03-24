876c814c55c9705d40f89d0c4ffcc1661525fa00
RichardOkello2 <okellor2@gmail.com>
2023-03-24 12:40:55 -0700

    Task system

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,6 @@
-package { 'python3-pip':
-  ensure => installed,
-}
+# Installs puppet-lint
 
-exec { 'install-flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
-
