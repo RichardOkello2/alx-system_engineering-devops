c367f7434a5e6745d844c51884e068a21fb405cc
RichardOkello2 <okellor2@gmail.com>
2023-03-24 12:22:33 -0700

    System

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,6 @@
-# install puppet-lint -v 2.5.0 
-  
- exec { 'puppet-lint': 
-   command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# Installs puppet-lint
+
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
+}
